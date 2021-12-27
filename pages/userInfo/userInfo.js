563f5e14221e2283f525354abe5ff70d80572b09
tx <896518640@qq.com>
2021-12-27 22:34:34 +0800

    完善了静态页面

---
+++ pages/userInfo/userInfo.js
@@ -0,0 +1,120 @@
+// pages/personal/personal.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    canIUseGetUserProfile: false,
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName')
+  },
+  getData(url, method, data = {}, header = {}) {
+    return new Promise(function (resolve, reject) {
+      wx.request({
+        url: url,
+        data: data,
+        method: method,
+        header: header,
+        success: function (res) {
+          if (res.statusCode == 200) {
+            resolve(res.data);
+          } else {
+            reject('error');
+          }
+        },
+        fail: function (e) {
+          reject(e);
+        }
+      })
+    })
+  },
+  getBannerData: function () {
+    this.getData('http://apis.juhe.cn/fapig/douyin/billboard', 'get', {
+      key: 'a45074759fde9f8da6a53be7358aaf65',
+      type: 'amusement_overall'
+    }).then(res => {
+      console.log(res)
+    },e => {
+      console.log(e)
+    });
+
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res);
+        console.log(e);
+        console.log(res.userInfo)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    console.log(this.data.userInfo);
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ utils/data.js
@@ -0,0 +1,17 @@
+//想要导出数据 就必须要定义变量 用exports
+var bannerData = [{
+  id: 1,
+  banner_img: "https://img2.baidu.com/it/u=155163597,4137996389&fm=26&fmt=auto"
+}, {
+  id: 2,
+  banner_img: "https://img0.baidu.com/it/u=2755313968,2418553549&fm=26&fmt=auto"
+},
+{
+  id: 3,
+  banner_img: "https://img2.baidu.com/it/u=3634083367,4087626788&fm=26&fmt=auto"
+}
+];
+
+module.exports = {
+  bannerData: bannerData
+}
